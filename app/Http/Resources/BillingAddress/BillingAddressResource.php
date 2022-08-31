5ee3ced6565d378bb8643c928a0cc582cf80463c
“fullstackprofile” <fullstackprofile@gmail.com>
2022-08-31 19:32:34 +0400

    comment in billingaddress

--- app/Http/Resources/BillingAddress/BillingAddressResource.php
+++ app/Http/Resources/BillingAddress/BillingAddressResource.php
@@ -18,7 +18,7 @@ class BillingAddressResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'country_region'=>$this->company->country_region,
+//            'country_region'=>$this->company->country_region,
             'street_address'=>$this->company->street_address,
             'town_city'=>$this->company->town_city,
             'postcode_zip'=>$this->company->postcode_zip,
