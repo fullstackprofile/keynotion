ba4ba446adef8b0783d371665477b191b4eee2b1
“fullstackprofile” <fullstackprofile@gmail.com>
2022-09-06 11:55:10 +0400

    Stripe

--- bootstrap/cache/packages.php
+++ bootstrap/cache/packages.php
@@ -17,6 +17,13 @@
       'Image' => 'Intervention\\Image\\Facades\\Image',
     ),
   ),
+  'laravel/cashier' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Cashier\\CashierServiceProvider',
+    ),
+  ),
   'laravel/sail' => 
   array (
     'providers' => 
